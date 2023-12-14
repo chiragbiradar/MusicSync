956c71d0f6299b4e580296114cce64c449b83172
Chirag Biradar <chiragsb16@gmail.com>
2023-12-14 20:46:19 +0000

    jfjf

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
 
 # Create your views here.
-def main(request):
-    return HttpResponse("<h1>Hello</h1>")
+def RoomView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
